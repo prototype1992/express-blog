97ecb13d1973d166bafdcd417d14d38e399592ca
Shamil <a_shoma@mail.ru>
2018-06-14 23:56:53 +0300

    настройка mongoose и создание модели для статей

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const keys = require('./keys');
 
 const port = process.env.PORT || 5000;
 const clientPath = path.join(__dirname, 'client');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI)
+    .then(response => {
+        console.log('MongoDB Connected!', response);
+    })
+    .catch(error => {
+        console.log('MongoDB Error!', error);
+    });
 
 // делаем папку client статичной
 app.use(express.static(clientPath));
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://shamil:prototype123pass@ds261040.mlab.com:61040/express-blog'
+};
